29a4e59385657e9d328369b92726d77d72d163ad
Andrew Marek <andwmarek@gmail.com>
2023-09-21 17:01:11 -0700

    some quick updates to formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,49 +28,43 @@
       paceKm = (totalMinutes / distanceKm).toFixed(2);
     } else {
       paceKm = "Please enter a distance greater than zero";
-      paceMile = "Please enter a distance greater than zero";
+      formattedPace = "Please enter a distance greater than zero";
     }
 </script>
 
-<body class="flex bg-zinc-50 items-stretch min-h-screen">
-    <div class="flex flex-col w-full">  
-        <nav class="flex gap-10 p-3 justify-between bg-white shadow-md">
-            <h1 class="text-lg font-semibold hover:text-indigo-500 cursor-pointer">andmarek</h1>
+<body class="flex bg-zinc-50 items-center justify-center min-h-screen">
+    <div class="flex flex-col w-96  bg-white p-6 rounded-lg shadow-md overflow-y-auto">  
+        <nav class="flex gap-6 mb-6 justify-between">
+            <h1 class="text-lg font-semibold hover:text-indigo-500 cursor-pointer"><a href="https://andmarek.com">andmarek</a></h1>
             <h1 class="text-lg font-semibold">Pace Calculator</h1>
             <h1 class="text-lg font-semibold hover:text-indigo-500 cursor-pointer">About</h1>
         </nav>
 
-        <div class="flex flex-col p-6">
-            <div id="input" class="flex flex-col space-y-4">
-                <label class="block text-sm font-medium text-gray-700" for="distance">
-                    Distance (mi):
-                    <input id="distance" class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={distanceMi} step="0.01" />
-                </label>
+        <div id="input" class="flex flex-col space-y-4">
+            <label class="block text-sm font-medium text-gray-700" for="distance">
+                Distance (mi):
+                <input id="distance" class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={distanceMi} step="0.01" />
+            </label>
 
-                <label class="block text-sm font-medium text-gray-700" for="hours">
-                    Hours:
-                    <input class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={hours} />
-                </label>
+            <label class="block text-sm font-medium text-gray-700" for="hours">
+                Hours:
+                <input class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={hours} />
+            </label>
 
-                <label class="block text-sm font-medium text-gray-700">
-                    Minutes:
-                    <input class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={minutes} />
-                </label>
+            <label class="block text-sm font-medium text-gray-700">
+                Minutes:
+                <input class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={minutes} />
+            </label>
 
-                <label class="block text-sm font-medium text-gray-700">
-                    Seconds:
-                    <input class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={seconds} />
-                </label>
-            </div>
-            
-            <button class="mt-4 bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50">
-                Calculate
-            </button>
+            <label class="block text-sm font-medium text-gray-700">
+                Seconds:
+                <input class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={seconds} />
+            </label>
+        </div>
 
-            <div id="output" class="flex flex-col mt-6">
-                <p class="block text-sm font-medium text-gray-700">Your pace (minutes per kilometer): {paceKm}</p>
-                <p class="block text-sm font-medium text-gray-700">Your pace (minutes per mile): {formattedPace}</p>
-            </div>
+        <div id="output" class="flex flex-col mt-6">
+            <p class="block text-sm font-medium text-gray-700">Your pace (minutes per kilometer): {paceKm}</p>
+            <p class="block text-sm font-medium text-gray-700">Your pace (minutes per mile): {formattedPace}</p>
         </div>
     </div>
 </body>
