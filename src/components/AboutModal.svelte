20c8c61520245b97ece69c3177e30157255793db
Andrew Marek <andwmarek@gmail.com>
2023-09-23 13:50:33 -0700

    small updates to usability

---
+++ src/components/AboutModal.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { fade } from 'svelte/transition';
+
+    export let showModal = false;
+    export let close: () => void;
+    if (showModal) {
+        console.log("wow");
+    };
+
+</script>
+{#if showModal}
+    <div transition:fade class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+        <div class="bg-white p-8 rounded-lg shadow-md">
+            <h2 class="text-2xl mb-4">PaceQuick - A Simple Running Tool</h2>
+            <p> </p>
+            <button on:click={close} class="mt-4 px-4 py-2 bg-indigo-500 text-white rounded">Close</button>
+        </div>
+  </div>
+{/if}
+
+<style>
+    
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script>
-    let distanceMi = 0; // Set initial value to avoid division by zero
+  import { text } from '@sveltejs/kit';
+    import Modal from '../components/AboutModal.svelte';
+
+    /* Constants */
+    const MILES_TO_KM = 1.60934; // conversion factor
+
+    let showModal = false;
+    let textDistance = 0; // Set initial value to avoid division by zero
     let hours = 0;
     let minutes = 0;
     let seconds = 0;
@@ -13,22 +20,50 @@
     /**
      * @type {string}
      */
-    let paceMile;
+    let pace;
+    let unit = 'miles'; // default unit
+    let textUnit = 'mi';
 
-    $: if (distanceMi > 0) {
-      let totalMinutes = (hours * 60) + minutes + (seconds / 60);
-      paceMile = (totalMinutes / distanceMi).toFixed(2);
+    /* Handlers */
+    const handleUnitChange = (event) => {
+        unit = event.target.value;
+        textUnit = unit === 'miles' ? 'mi' : 'km';
 
-      let mins = Math.floor(parseFloat(paceMile));
+        /* Handle if the unit changes and the user already
+            has entered a distance */
+        if (textDistance > 0) {
+            if (unit == "miles") {
+                textDistance = (textDistance / MILES_TO_KM);
+            } else {
+                textDistance = textDistance * MILES_TO_KM;
+            }
+        }
+        
+    };
+    // computed
 
-      const secs = parseFloat((seconds / distanceMi).toFixed(1));
-      formattedPace = `${mins}:${secs < 10 ? '0' : ''}${secs}`;
 
-      let distanceKm = distanceMi / 0.621371;
-      paceKm = (totalMinutes / distanceKm).toFixed(2);
-    } else {
-      paceKm = "Please enter a distance greater than zero";
-      formattedPace = "Please enter a distance greater than zero";
+    $: {
+    console.log("calculating pace")
+    if (textDistance > 0) {
+            const totalMinutes = (hours * 60) + minutes + (seconds / 60);
+            const distance = textDistance;
+            let paceMinutes = 0;
+            if (textUnit == "mi") {
+                paceMinutes = totalMinutes / textDistance;
+            } else if (textUnit == "km") {
+                let distanceKm = distance * MILES_TO_KM;
+                paceMinutes = totalMinutes / distanceKm;
+            }
+
+            // Format the pace
+            const mins = Math.floor(paceMinutes);
+            const secs = Math.round((paceMinutes - mins) * 60 * 10) / 10; // one decimal place
+            formattedPace = `${mins.toString().padStart(2, '0')}:${secs.toFixed(1).padStart(4, '0')}`;
+
+        } else {
+            formattedPace = "Please enter a distance greater than zero";
+        }
     }
 </script>
 
@@ -37,15 +72,27 @@
         <nav class="flex gap-6 mb-6 justify-between">
             <h1 class="text-lg font-semibold hover:text-indigo-500 cursor-pointer"><a href="https://andmarek.com">andmarek</a></h1>
             <h1 class="text-lg font-semibold">Pace Calculator</h1>
-            <h1 class="text-lg font-semibold hover:text-indigo-500 cursor-pointer">About</h1>
+            <button 
+        class="basis-1/2 text-left bg-transparent border-none cursor-pointer p-0 focus:outline-none"
+        on:click={() => showModal = true}
+    >
+        About
+    </button>
         </nav>
-
+        <label>
+            <input type="radio" value="miles" bind:group={unit} on:change={handleUnitChange} />
+            Miles
+          </label>
+          
+          <label>
+            <input type="radio" value="km" bind:group={unit} on:change={handleUnitChange} />
+            Kilometers
+          </label>
         <div id="input" class="flex flex-col space-y-4">
             <label class="block text-sm font-medium text-gray-700" for="distance">
-                Distance (mi):
-                <input id="distance" class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={distanceMi} step="0.01" />
+                Distance ({textUnit}):
+                <input id="distance" class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={textDistance} step="0.01" />
             </label>
-
             <label class="block text-sm font-medium text-gray-700" for="hours">
                 Hours:
                 <input class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" type="number" bind:value={hours} />
@@ -63,8 +110,8 @@
         </div>
 
         <div id="output" class="flex flex-col mt-6">
-            <p class="block text-sm font-medium text-gray-700">Your pace (minutes per kilometer): {paceKm}</p>
-            <p class="block text-sm font-medium text-gray-700">Your pace (minutes per mile): {formattedPace}</p>
+            <p class="block text-sm font-medium text-gray-700">Your pace (minutes per {textUnit}): {formattedPace}</p>
         </div>
     </div>
+    <Modal {showModal} close={() => showModal = false} />
 </body>
